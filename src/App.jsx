8f9c59ebdc79bd2d23ffa175768eef2f12017118
Pawan-CEO <mailpawanmehra@gmail.com>
2022-09-20 02:23:31 +0530

    scss chagnes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Board from "./components/Board";
+import './styles/root.scss';
 const App =()=>{
   return (
-    <div>
+    <div className="app">
     <h2>Tic Tac Toe</h2>
     <Board/>
     </div>

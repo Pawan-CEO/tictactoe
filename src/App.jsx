4762d7a095d31552affe91d9741283b5be7f72fc
Pawan-CEO <mailpawanmehra@gmail.com>
2022-09-20 01:16:49 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./components/Board";
+const App =()=>{
+  return (
+    <div>
+    <h2>Tic Tac Toe</h2>
+    <Board/>
+    </div>
+  );
+};
+
+export default App;
